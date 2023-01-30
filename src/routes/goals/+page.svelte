dc87193dd97b70b1352a21c7b0902ab5b0aeccbf
Romain Durand <romain.durand@radiofrance.com>
2023-01-30 08:00:15 +0100

    plug goals to SSE api, start goals list

--- src/routes/goals/+page.svelte
+++ src/routes/goals/+page.svelte
@@ -1,54 +1,44 @@
 <script>
-	let donations = 29000;
-	/**
-	 * @typedef Goal
-	 * @type {Object}
-	 * @property {string} text
-	 * @property {number} value
-	 */
+	import { onMount } from 'svelte';
+	import goals from './goals.js';
+
+	let donations = 0;
+
+	$: ({ percent, nextGoal } = updateNextGoalData(donations, goals));
+
+	onMount(async () => {
+		const response = await window.fetch('https://piquetdestream-api.fly.dev/v1/counter/state');
+		const data = await response.json();
+		donations = Number.parseFloat(data.amount);
+		const evtSource = new EventSource('https://piquetdestream-api.fly.dev/v1/counter/sse');
+		evtSource.addEventListener('counter-update', handleEvent);
+	});
+
+	function handleEvent(event) {
+		const data = JSON.parse(event.data);
+		donations = Number.parseFloat(data.amount);
+	}
 
 	/**
-	 * @type Goal[]
-	 */
-	let goals = [
-		{
-			text: 'objectif 1',
-			value: 30000
-		},
-		{
-			text: 'objectif 2',
-			value: 31000
-		},
-		{
-			text: 'objectif 3',
-			value: 32000
-		},
-		{
-			text: 'objectif 1.5',
-			value: 29500
-		}
-	];
-
-	$: percent = getPercent(donations, goals);
-
-	/**
-	 *
 	 * @param {number} donations
-	 * @param {Goal[]} goals
 	 */
-	function getPercent(donations, goals) {
+	function updateNextGoalData(donations) {
 		const sortedGoals = goals.sort((a, b) => a.value - b.value);
 		const nextGoal = sortedGoals.find((goal) => goal.value > donations);
-		const nextGoalIndex = sortedGoals.findIndex(goal => goal.value === nextGoal.value);
-		const prevGoal = sortedGoals[nextGoalIndex - 1]
-
+		const nextGoalIndex = sortedGoals.findIndex((goal) => goal.value === nextGoal.value);
+		const prevGoal = sortedGoals[nextGoalIndex - 1] || { value: 0 };
+		const percent = ((donations - prevGoal.value) / (nextGoal.value - prevGoal.value)) * 100;
+		return { percent, nextGoal };
 	}
 </script>
 
 <div class="wrapper">
 	<div class="container">
-		<div class="progress" style={`width: 50%;`} />
-		<div class="next-goal-value">30000</div>
+		<div class="progress" style={`width: ${percent}%;`} />
+		<div class="text-content">
+			<div class="next-goal-title">{nextGoal.text}</div>
+			<div class="next-goal-value">{nextGoal.value}</div>
+		</div>
 	</div>
 </div>
 
@@ -58,30 +48,43 @@
 	}
 	.container {
 		position: relative;
-		box-sizing: content-box;
+		overflow: hidden;
 		width: 100%;
 		height: 30px;
 		border: 2px solid black;
-		border-radius: 3px;
+		border-radius: 10px;
 		background-color: white;
 	}
 
 	.progress {
 		position: absolute;
-		left: 0;
-		top: 0;
-		background-color: blueviolet;
-		border-right: 1px solid black;
+		background-color: #9a0022;
+		border-right: 1px solid red;
 		height: 100%;
+		transition: width 1s ease-in-out;
+	}
+
+	.text-content {
+		box-sizing: border-box;
+		padding-left: 10px;
+		padding-right: 10px;
+		width: 100%;
+		position: absolute;
+		display: flex;
+		justify-content: space-between;
 	}
 
 	.next-goal-value {
 		font-family: 'Nerko One';
-		position: absolute;
-		right: 5px;
-		top: 0;
 		color: white;
 		text-shadow: 0 0 2px black, 0 0 3px black, 0 0 4px black;
 		font-size: 1.6em;
+	}
+
+	.next-goal-title {
+		font-family: 'Nerko One';
+		color: white;
+		text-shadow: 0 0 2px black, 0 0 3px black, 0 0 4px black;
+		font-size: 1.4em;
 	}
 </style>
---
+++ src/routes/goals/goals.js
@@ -0,0 +1,56 @@
+/**
+ * @typedef Goal
+ * @type {Object}
+ * @property {string} text
+ * @property {number} value
+ */
+
+/**
+ * @type Goal[]
+ */
+const goals = [
+	// https://twitter.com/Yukain0/status/1617713674835361794
+	{
+		text: "Yukain0 ouvre un Only Fan (mais c'est pas ce que vous croyez)",
+		value: 35500
+	},
+	{
+		text: "Yukain0 publie sa vidéo d'elle enfant qui chante Le Roi Lion",
+		value: 40000
+	},
+	{
+		text: 'Yukain0 fait un suplex à Joyeux Sociologue (vidéo)',
+		value: 50000
+	},
+	{
+		text: 'Yukain0 va a la prochaine manif sur Paris avec Joyeux Sociologue',
+		value: 55000
+	},
+	{
+		text: "Yukain0 essaye de faire une interview d'Elise Lucet",
+		value: 100000
+	},
+	// https://twitter.com/PiquetDeStream/status/1617873404618760192/photo/2
+	{
+		text: 'Joyeux Sociologue lit du Gérald Bronner en LIVE (pire sociologue EVER)',
+		value: 32000
+	},
+	{
+		text: 'Remix DJ set DNB/jungle des chansons de campagne du RPR',
+		value: 33000
+	},
+	{
+		text: 'Joyeux Sociologue devient anti-Bourdieu',
+		value: 34000
+	},
+	{
+		text: "Joyeux Sociologue et Echaarpe regardent les débats à l'AN pendant une journée",
+		value: 35000
+	},
+	{
+		text: 'LeoJak0tte fait un selfie catboy',
+		value: 36000
+	}
+];
+
+export default goals;
